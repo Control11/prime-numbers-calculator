c81be05a5961d8e61039694d63d128310bafe406
Control11 <Control11@gmail.com>
2023-07-18 14:08:54 +0200

    initialize project

---
+++ src/main/java/com/example/prime_numbers_calculator/PrimeNumbersCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.prime_numbers_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumbersCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumbersCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prime_numbers_calculator/PrimeNumbersCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prime_numbers_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumbersCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
